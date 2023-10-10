62ada6489a9099502e30366a02ea17f2dff995cf
jsbaker628 <jessica.spach@gmail.com>
2023-10-09 22:35:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(".logo").animate({
+   opacity: 1
+ }, 8000)
+ $(".favorites").animate({
+   opacity: 1
+ }, 8000)
+$(".quarterly").animate({
+   opacity: 1
+ }, 8000)
+$(".location-container").animate({
+   opacity: 1
+ }, 8000)
+$(".hours-container").animate({
+   opacity: 1
+ }, 8000)
+
+
+// logo
+// favorites
+// quarterly
+// location
+// hours
